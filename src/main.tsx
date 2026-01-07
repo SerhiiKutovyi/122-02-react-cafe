494f4b6f776d54323565c38a005db5c396dd4b57
SerhiiKutovyi <wagenrex@gmail.com>
2026-01-07 20:45:29 +0200

    ok

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById('root')!).render(
+import App from './App.tsx';
+
+import 'modern-normalize';
+import './index.css';
+
+createRoot(document.getElementById('root') as HTMLDivElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
